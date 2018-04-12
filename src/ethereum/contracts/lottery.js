43b34e6bd30b20d9034cbc453c6e5b4cae26e1a6
Thomas Zhu <dev@thomaszhu.com>
2018-04-12 14:03:23 -0700

    Hook up to the lottery contract on Rinkeby

---
+++ src/ethereum/contracts/lottery.js
@@ -0,0 +1,59 @@
+import web3 from '../web3';
+
+const abi = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'manager',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'pickWinner',
+    outputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'getPlayers',
+    outputs: [{ name: '', type: 'address[]' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [],
+    name: 'enter',
+    outputs: [],
+    payable: true,
+    stateMutability: 'payable',
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [{ name: '', type: 'uint256' }],
+    name: 'players',
+    outputs: [{ name: '', type: 'address' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    payable: false,
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+];
+
+const address = '0x065F48CA27c24F31e911C4a4E5218B6E3FC5e433';
+
+export default new web3.eth.Contract(abi, address);
